8c0041e5fbbb07fefca4b84d7c964bced2f52ea5
Issam Zantout <80976696+izantout@users.noreply.github.com>
2021-09-12 16:57:25 -0700

    Update code.py

--- Time Adder/code.py
+++ Time Adder/code.py
@@ -1,16 +1,16 @@
-days = float(input('How many inputs do you want to have? '))
+input_number = float(input('How many inputs do you want to have? '))
 total_hours = total_minutes = total_seconds = 0
-while days % 1 != 0:
-    days = float(input('Invalid entry. Please try again'))
+while input_number % 1 != 0:
+    input_number = float(input('Invalid entry. Please try again'))
 else:
-    for i in range(int(days)):
-        hours = int(input('How many hours did you want to add for that day '))
+    for i in range(int(input_number)):
+        print('Day', i + 1)
+        hours = int(input('How many hours did you want to add for this day'))
         total_hours = hours + total_hours
-        minutes = int(input("How many minutes did you want to add for that day "))
+        minutes = int(input("How many minutes did you want to add for this day "))
         total_minutes = minutes + total_minutes
-        seconds = int(input("How many seconds did you want to add for that day "))
+        seconds = int(input("How many seconds did you want to add for this day "))
         total_seconds = seconds + total_seconds
-
 
 while total_seconds >= 60:
     total_minutes = total_minutes + 1
@@ -27,6 +27,6 @@ minutes_in_seconds = total_minutes * 60
 final_minutes = total_minutes + hours_in_minutes
 final_seconds = total_seconds + hours_in_seconds + minutes_in_seconds
 
-print('In total, there are', total_hours, 'hours', total_minutes, 'minutes and ', total_seconds, 'seconds.')
+print('In total, there are', total_hours, 'hours', total_minutes, 'minutes and', total_seconds, 'seconds.')
 print('Another way of showing this is ', final_minutes, 'minutes and', total_seconds, 'seconds', 'or', final_seconds,
       'seconds')
