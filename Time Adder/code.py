700ec3d9150c3a43ba6ce0024c472b1b32418112
Issam Zantout <80976696+izantout@users.noreply.github.com>
2021-09-13 19:35:03 -0700

    Update code.py

--- Time Adder/code.py
+++ Time Adder/code.py
@@ -5,7 +5,7 @@ while input_number % 1 != 0:
 else:
     for i in range(int(input_number)):
         print('Day', i + 1)
-        hours = int(input('How many hours did you want to add for this day'))
+        hours = int(input('How many hours did you want to add for this day '))
         total_hours = hours + total_hours
         minutes = int(input("How many minutes did you want to add for this day "))
         total_minutes = minutes + total_minutes
@@ -13,12 +13,12 @@ else:
         total_seconds = seconds + total_seconds
 
 while total_seconds >= 60:
-    total_minutes = total_minutes + 1
-    total_seconds = total_seconds - 60
+    total_minutes += 1
+    total_seconds -= 60
 
 while total_minutes >= 60:
-    total_hours = total_hours + 1
-    total_minutes = total_minutes - 60
+    total_hours += 1
+    total_minutes -= 60
 
 hours_in_minutes = total_hours * 60
 hours_in_seconds = total_hours * 3600
@@ -30,3 +30,6 @@ final_seconds = total_seconds + hours_in_seconds + minutes_in_seconds
 print('In total, there are', total_hours, 'hours', total_minutes, 'minutes and', total_seconds, 'seconds.')
 print('Another way of showing this is ', final_minutes, 'minutes and', total_seconds, 'seconds', 'or', final_seconds,
       'seconds')
+
+print(' Hours                            Minutes                            Seconds')
+print('  ',total_hours,'                               ',total_minutes,'                                ',total_seconds)
