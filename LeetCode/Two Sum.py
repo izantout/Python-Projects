4df3ba3ffe57abe95e83c7bfaddacb3b4867a253
Issam Zantout <80976696+izantout@users.noreply.github.com>
2021-09-15 21:26:03 -0700

    Update Two Sum.py

--- LeetCode/Two Sum.py
+++ LeetCode/Two Sum.py
@@ -1,10 +1,5 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
         nums2 = []
         for i in range(len(nums)):
             for j in range(i + 1, len(nums)):
