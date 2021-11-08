0504a06d5fd37c14ab6e7ab1e0756ffbbfec9eb8
Issam Zantout <80976696+izantout@users.noreply.github.com>
2021-11-08 13:16:40 -0800

    Update Reverse Integer.py

--- LeetCode/Medium/Reverse Integer.py
+++ LeetCode/Medium/Reverse Integer.py
@@ -4,15 +4,15 @@ class Solution(object):
         y = 0
         z = 0
 
-        while x > 0 or x < 0:
+        while x > 0 or x < 0: # checks values for x != 0 and reverses them using absolute value
             z = abs(x) % 10
             y = y * 10 + z
             x = abs(x) // 10
 
         if x2 < 0:
-            y = y * (-1)
+            y = y * (-1) # Checks if original number is negative to multiply by negative one since we flipped the absolute value
 
-        if -2**31 < y < (2**31)-1:
+        if -2**31 < y < (2**31)-1: # Checks if the number is a 32 bit number
             y = y
         else:
             y = 0
