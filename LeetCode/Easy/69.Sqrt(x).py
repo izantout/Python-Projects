341eda4bf489fd42c221b30787cb66bc395ee954
Issam Zantout <80976696+izantout@users.noreply.github.com>
2021-12-02 19:30:52 -0800

    Create 69.Sqrt(x).py

---
+++ LeetCode/Easy/69.Sqrt(x).py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        return int(sqrt(x))
+      
+      #Runtime: 12 ms, faster than 99.08% of Python online submissions for Sqrt(x).
+      #Memory Usage: 13.5 MB, less than 37.57% of Python online submissions for Sqrt(x).
